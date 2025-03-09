1ce62bc4279a3ba6afe98f1e0d2ff649f9929df7
ken mcfadden <kendmcfadden@gmail.com>
2025-03-09 08:03:20 -0700

    page 33 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
+const welcome =  {
+  greeting: 'Hey',
+  title: 'Dude...',
+    where: "Where's my Car?"
+}
+const list = [
+    {
+        title: 'React',
+        url: 'https://reactjs.org/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+    },
+    {
+        title: 'Redux',
+        url: 'https://redux.js.org/',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+    },
+];
 function App() {
-
   return (
       <div>
-          Hello Dude!
+        <h1> {welcome.greeting} {welcome.title} {welcome.where} </h1>
+        <label htmlFor={"search"}>Search: </label>
+        <input id="search" type={"text"}/>
+      <hr />
+      <ul style={{ 
+        listStyle: "none", 
+        padding: 0,
+        textAlign: "left",
+        display: "flex",
+        flexDirection: "column",
+        gap: "10px"
+      }}>
+          {list.map(function (item) {
+              return (
+                <li key={item.objectID}>
+                    <a href={item.url}>Title: {item.title}</a>
+                    <div>Author: {item.author}</div>
+                    <div>Comments: {item.num_comments}</div>
+                    <div>Points: {item.points}</div>
+                </li>
+            );
+          })}
+      </ul>
       </div>
-  )
+  );
 }
 
-export default App
+export default App;
